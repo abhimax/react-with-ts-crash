17658c90f909693f077da0d6a1e405776865896e
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-06-22 08:57:14 +0530

    feat: add counter

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer';
 import Employee from './components/Employee';
+import Counter from './components/Counter';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <i className="fa fa-home fa-2x"></i>
       <Customer name="John" age={26} title="Frontend Developer"/>
       <Employee name="Tim" age={38} title="CEO"/>
+      <Counter></Counter>
     </React.Fragment>
   );
 }
---
+++ src/components/Counter.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+interface IProps{}
+interface IStates{
+    count : number,
+}
+
+let Counter:React.FC<IProps> = () => {
+    let [state, setState] = useState<IStates>({
+        count : 0
+    });
+
+let increment = ():void =>{
+setState(
+    {
+        count : state.count + 1
+    }
+)
+}
+
+let decrement = ():void => {
+    setState(
+        {
+           count : state.count - 1 
+        }
+    )
+}
+    return(
+        <React.Fragment>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4">
+                        <div className="card">
+                            <div className="card-body">
+                                <p className="h3">{state.count}</p>
+                                <button className="btn btn-success m-1" onClick={increment}>Increment</button>
+                                <button className="btn btn-danger m-1" onClick={decrement}>Decrement</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+};
+export default Counter;
